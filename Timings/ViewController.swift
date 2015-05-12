5be4c7c29af60e1094d72300fbfdd2c2376e6425
Nathan Corvino <nathan@corvino.com>
2015-05-12 13:38:12 -0500

    Initial project created by Xcode 6.3.

---
+++ Timings/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Timings
+//
+//  Created by Nathan Corvino on 5/12/15.
+//  Copyright (c) 2015 Nathan Corvino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
